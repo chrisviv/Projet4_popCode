89dcd7ef2aaed7e95fbf71ab46ac017ca6c22185
chris <c.vivancos@codeur.online>
2022-06-28 16:15:33 +0200

    debut projet

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,17 @@
-let loading = document.querySelector("#title") ;
-let wrapper = document.querySelector(".wrapper");
-let newPage = document.querySelector("#display") ;
+// let loading = document.querySelector("#title") ;
+// let wrapper = document.querySelector(".wrapper");
+// let newPage = document.querySelector("#display");
+// let body = document.querySelector("body");
+// let para = document.queryCommandValue("p");
 
 
-window.onload = setTimeout(showContent, 15000);
+// window.onload = setTimeout(showContent, 10000);
 
-function showContent() {
-  loading.style.display = "none";
-  wrapper.style.display = "none";
-  newPage.style.display = "block";
-}
+// function showContent() {
+//   loading.style.display = "none";
+//   wrapper.style.display = "none";
+//   newPage.style.display = "block";
+//   body.style.background = "black";
+//   para.style.display = "block";
+
+// }
