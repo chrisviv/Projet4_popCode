3508e4536336def1e08db04bdcf506195bca76d9
chris <c.vivancos@codeur.online>
2022-06-28 16:28:37 +0200

    loader

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,17 @@
-// let loading = document.querySelector("#title") ;
-// let wrapper = document.querySelector(".wrapper");
-// let newPage = document.querySelector("#display");
-// let body = document.querySelector("body");
-// let para = document.queryCommandValue("p");
+let loading = document.querySelector("#title") ;
+let wrapper = document.querySelector(".wrapper");
+let newPage = document.querySelector("#display");
+let body = document.querySelector("body");
+let para = document.queryCommandValue("p");
 
 
-// window.onload = setTimeout(showContent, 10000);
+window.onload = setTimeout(showContent, 10000);
 
-// function showContent() {
-//   loading.style.display = "none";
-//   wrapper.style.display = "none";
-//   newPage.style.display = "block";
-//   body.style.background = "black";
-//   para.style.display = "block";
+function showContent() {
+  loading.style.display = "none";
+  wrapper.style.display = "none";
+  newPage.style.display = "block";
+  body.style.background = "black";
+  para.style.display = "block";
 
-// }
+}
