1228581abcf3ed71ea5919e22d528918086129e7
chris <c.vivancos@codeur.online>
2022-06-29 22:11:33 +0200

    corriger erreur d affichage background

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,36 @@
 let loading = document.querySelector("#title") ;
 let wrapper = document.querySelector(".wrapper");
-let newPage = document.querySelector("#display");
+let display = document.querySelector("#display");
 let body = document.querySelector("body");
 let para = document.queryCommandValue("p");
 
 
-window.onload = setTimeout(showContent, 10000);
+window.onload = setTimeout(showContent, 6000);
 
 function showContent() {
   loading.style.display = "none";
   wrapper.style.display = "none";
-  newPage.style.display = "block";
-  body.style.background = "black";
-  para.style.display = "block";
+  // nouvelle page apparait
+  display.style.display = "block";
+  display
+  body.style.background = "block";
+    para.style.display = "block";
 
 }
+let logo = document.querySelector('.logo-code');
+let legales = document.querySelector('.legales');
+let politique = document.querySelector('.politque');
+
+window.onload = setTimeout(showContent1, 6000);
+
+function showContent1() {
+  logo.style.display = "none";
+  legales.style.display = "block";
+  politique.style.display = 'block';
+
+}
+let code = document.getElementById('code')
+
+window.setTimeout(()=>{
+  code.style.display = "none"
+}, 6000)
