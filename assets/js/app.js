67ed62355d7c6741b1c76d143b9bfc4048bb0177
chris <c.vivancos@codeur.online>
2022-07-02 16:00:36 +0200

    modifcation html chargement

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,36 +1,29 @@
-let loading = document.querySelector("#title") ;
+let title = document.querySelector("#title") ;
 let wrapper = document.querySelector(".wrapper");
-let display = document.querySelector("#display");
+let display = document.querySelector(".display");
 let body = document.querySelector("body");
-let para = document.queryCommandValue("p");
+let logo = document.querySelector('.logo-code');
+let number = document.querySelector('.number');
+let reference = document.querySelector('.reference');
+let commerce = document.querySelector('.commencer');
 
-
-window.onload = setTimeout(showContent, 6000);
+ window.onload = setTimeout(showContent, 6000);
 
 function showContent() {
-  loading.style.display = "none";
+  title.style.display = "none";
   wrapper.style.display = "none";
-  // nouvelle page apparait
-  display.style.display = "block";
-  display
   body.style.background = "block";
-    para.style.display = "block";
-
-}
-let logo = document.querySelector('.logo-code');
-let legales = document.querySelector('.legales');
-let politique = document.querySelector('.politque');
-
-window.onload = setTimeout(showContent1, 6000);
-
-function showContent1() {
   logo.style.display = "none";
-  legales.style.display = "block";
-  politique.style.display = 'block';
+  display.style.display = "block";
+  number.style.display = "block";
+  reference.style.display = "block";
+  commerce.style.display = "block";
 
-}
-let code = document.getElementById('code')
+ }
+// // 2eme section
+// // display nouvelle page 
 
-window.setTimeout(()=>{
-  code.style.display = "none"
-}, 6000)
+
+// //  let code = document.getElementById('code')
+// //  window.setTimeout(()=>{
+// //   code.style.display = "none" }, 6000)
