ac0ce06ff5029d270e1dd8b30a91d99a17651510
chris <c.vivancos@codeur.online>
2022-07-03 12:08:16 +0200

    avancé la partie intégration Jeu

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,24 @@
 let title = document.querySelector("#title") ;
 let wrapper = document.querySelector(".wrapper");
-let boxChargement = document.querySelector("#boxChargement");
-let loader = document.querySelector("#loader");
+let boxchargement = document.querySelector("#boxChargement");
+let loader= document.querySelector("#loader");
 let logo = document.querySelector('.logo-code');
 let chargement = document.querySelector('.chargement');
 
-
- window.onload = setTimeout(showContent, 6000);
+window.onload = setTimeout(showContent, 6000);
 
 function showContent() {
+  logo.style.display = "none";
   title.style.display = "none";
   wrapper.style.display = "none";
-  logo.style.display = "none";
   // nouvelle page apparait
   loader.style.display = "none";
-  boxChargement.style.display = "block";
+  boxchargement.style.display = "block";
   body.style.background = "block";
-  chargement.style.display = "block";
+  chargement.style.display ="block";
+}
 
- }
+
 
 
 // //  let code = document.getElementById('code')
