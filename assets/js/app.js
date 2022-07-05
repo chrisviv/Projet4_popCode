821e2a750b3fdb1a79e7eb6827d096436c7c1ea2
chris <c.vivancos@codeur.online>
2022-07-05 15:32:17 +0200

    debut affichage de nom en js

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,9 +17,53 @@ function showContent() {
   body.style.background = "block";
   chargement.style.display ="block"; }
 
+  // ***************************************************
+
+  let nomLangage = document.getElementById('nomLangage');
+  let tapeName = document.querySelector('.tapeName');
+  let boxJeu = document.querySelector('#boxJeu');
+    
+  tapeName.style.display= 'none';
+    
+  window.onload = setTimeout(showTape, 7000);
+  
+  function showTape(){
+    tapeName.style.display= 'block'; }
+
+// 
+  
+window.addEventListener('keydown', function(event){
+  key = event.key
+  console.log(key);
+  if(key === event.key){
+    nomLangage.focus()
+    nomLangage.classList.remove('opacite');
+  }
+})
+
+window.addEventListener('keydup', function(event){
+  key = event.key
+  console.log(key);
+  if(key === event.key){
+    nomLangage.focus()
+    nomLangage.classList.remove('opacite');
+  }
+})
 
 
 
-// //  let code = document.getElementById('code')
-// //  window.setTimeout(()=>{
-// //   code.style.display = "none" }, 6000)
+
+
+
+
+
+  // function controle(){
+  // let saisie= document.getElementById('nomLangage').value;
+  // alert("Vous avez saisi : "+ saisie);
+  
+  // for(let i=0; i<tapeName.length; i++){
+  //   let tapeClavier = tapeName[i];
+  //   tapeClavier.addEventListener('click', showWrite, false);
+  // }
+
+
---
+++ assets/js/list.js
@@ -0,0 +1,33 @@
+let listLangages =['JavaScript', 'javascript', 'JAVASCRIPT','HTML','html','CSS', 'css','SQL','sql','Python','python', 'PYTHON','JAVA','java','Java',
+'Bash','bash','BASH', 'Shell','SHELL','shell','PowerShell', 'POWERSHELL', 'powershell','C#', 'c#','PHP','php', 'C++', 'C++','TypeScript','TYPESCRIPT', 'typescript',
+'typeScript','C','c','Ruby','RUBY','ruby','Go','go','GO','Assembly','assembly','ASSEMBLY','Swift','swift','SWIFT','Kotlin','KOTLIN','kotlin','R','r','VBA','vba',
+'Objective-C','objective-c','OBJECTIVE-C','Scala','SCALA','scala','Rust','RUST','rust','Dart','DART','dart','Elixir','elixir','ELIXIR','Clojure','clojure','CLOJURE', 
+'WebAssembly', 'webassembly','WEBASSEMBLY'];
+
+
+// ***************************************************//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let nomLangage = document.querySelector('.nomLangage');
+// let nomLangages ='';
+// JUSTE POUR AFFICHER PAS RESULTAT DEMANDER
+// let i
+//         for (i = 0; i < nomLanage.length; i++) {
+//             nomLangages += "<li>" + listLangages[i].toUpperCase() + "</li>";
+//         }
+  
+//         document.querySelector(".nomLangage").innerHTML = nomLangages;
